b395bf84006b13786a9768d1102412dc1cefb5ed
Rolando Morales <rolando.moralesjr21@gmail.com>
2022-05-01 01:38:47 -0700

    deployment changes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,21 @@
-const express = require('express')
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
+
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, "..", "client", "build");
+
+console.log(publicPath)
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
+
 
