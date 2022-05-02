195ba9d462641d60cd72616c6b52ceeb24f25cd1
Rolando Morales <rolando.moralesjr21@gmail.com>
2022-05-01 19:32:43 -0700

    installed cors

--- server/server.js
+++ server/server.js
@@ -3,30 +3,33 @@ const jsonServer = require("json-server");
 const path = require("path");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema.js");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 
 const router = jsonServer.router("data.json");
-// const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 4000;
 const publicPath = path.join(__dirname, "..", "client", "build");
 
+app.use(cors());
 app.use("/api", router);
-// app.use(middlewares);
-// app.use(cors());
 
-// app.use(
-//   "/api/graphql",
-//   graphqlHTTP({
-//     schema: schema,
-//     graphiql: true,
-//   })
-// );
+console.log(router);
+
+app.use(middlewares);
+
+app.use(
+  "/api/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
 
 app.use(express.static(publicPath));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
