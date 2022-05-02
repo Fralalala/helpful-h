4bcb2fe1237c586477d42b8093e6f9b972c7eb46
Rolando Morales <rolando.moralesjr21@gmail.com>
2022-05-01 18:47:08 -0700

    Integrated graphql

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
 const express = require("express");
-var jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const path = require("path");
-
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema.js");
+const cors = require("cors");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const jsonServer = require("json-server");
+const router = jsonServer.router("data.json");
+const middlewares = jsonServer.defaults();
+
+// const app = jsonServer.create();
+
+const port = process.env.PORT || 4000;
 const publicPath = path.join(__dirname, "..", "client", "build");
 
+app.use(router);
+app.use(middlewares);
+app.use(cors());
 app.use(express.static(publicPath));
-app.use('/api', jsonServer.router('data.json'));
+
+app.use(
+  "/api/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
+app.use(express.static(publicPath));
+app.use("/api", jsonServer.router("data.json"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
