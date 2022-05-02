30e09e227874dbdc3aff1558bd689d723af7f7fe
Rolando Morales <rolando.moralesjr21@gmail.com>
2022-05-02 00:36:47 -0700

    Color endpoints

--- server/server.js
+++ server/server.js
@@ -1,30 +1,47 @@
 const express = require("express");
-const jsonServer = require("json-server");
 const path = require("path");
-const { graphqlHTTP } = require("express-graphql");
-const schema = require("./schema.js");
 const cors = require("cors");
+const { default: axios } = require("axios");
 const app = express();
 
-const router = jsonServer.router("./data.json");
-// const middlewares = jsonServer.defaults();
-
-const port = process.env.PORT || 4000;
 const publicPath = path.join(__dirname, "..", "client", "build");
+const port = process.env.PORT || 3000;
+const apiUrl = "https://hh-json-server.herokuapp.com/colors/";
 
 app.use(cors());
 app.use(express.json());
-app.use(router);
 
-// app.use(middlewares);
+// get all colors
+app.get("/api/colors", async (req, res) => {
+  try {
+    const { data: colors } = await axios.get(apiUrl);
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-  })
-);
+    res.send(colors);
+  } catch (error) {
+    console.error("Error on get /api/colors", error);
+    res.status(500).send(error);
+  }
+});
+
+// delete all colors
+app.delete("/api/colors", async (req, res) => {
+  try {
+    const { data: colors } = await axios.get(apiUrl);
+
+    const apiCalls = [];
+
+    colors.forEach((color) => {
+      apiCalls.push(axios.delete(apiUrl + color.id));
+    });
+
+    await Promise.all(apiCalls);
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Error on delete /api/colors", error);
+    res.status(500).send(error);
+  }
+});
 
 app.use(express.static(publicPath));
 
