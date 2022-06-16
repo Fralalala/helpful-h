b23d8dba49ccde8edc6c90d086b823f98c027073
Rolando Morales <rolando.moralesjr21@gmail.com>
2022-06-15 18:54:22 -1000

    Added aws test

--- client/src/components/Content/ListView/ListView.tsx
+++ client/src/components/Content/ListView/ListView.tsx
@@ -76,6 +76,9 @@ const ListView = () => {
           </motion.div>
         ))}
       </div>
+
+      <img src="https://randmsgbucket.s3.us-west-1.amazonaws.com/2cb59578-dd6a-49a7-9894-49e290c0a1cd.jpg" alt="alt_text_img" />
+          
     </div>
   );
 };
